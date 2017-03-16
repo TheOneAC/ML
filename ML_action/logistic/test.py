1ac32e6b89a54705b7d74321cac4461154457628
The One Ac <scuhss@gmail.com>
2017-03-16 20:59:03 +0800

    logistic

---
+++ ML_action/logistic/test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/pthon
+from numpy import *
+import logistic
+
+
+
+#dataArr, lableMAt = logistic.loadDataSet()
+
+#weights =  logistic.gradAscent(dataArr, lableMAt)
+
+#weights =  logistic.stocGradAscent0(array(dataArr), lableMAt)
+
+#weights =  logistic.stocGradAscent1(array(dataArr), lableMAt)
+#weights =  logistic.stocGradAscent1(array(dataArr), lableMAt,500)
+
+
+#logistic.plotBestFit(weights)
+logistic.multiTest()
